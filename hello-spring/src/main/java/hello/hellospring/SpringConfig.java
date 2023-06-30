fc1e39616b06bd03b320231fd34da4e4ea43778f
secretj <qkrwlsgud890@gmail.com>
2023-06-30 19:43:43 +0900

    230630 메모리->JDBC 변경 및 스프링 통합 테스트

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,14 +1,30 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+/**
+ * 수동으로 Bean등록을 하기위한 Config 클래스
+ */
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -16,6 +32,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+       // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
