d2c617ac36b47b3f7616c75863f96c5da53ece7b
secretj <qkrwlsgud890@gmail.com>
2023-07-25 20:33:41 +0900

    0725 제네릭 타입의 공통 인터페이스 추가 및 테스트

---
+++ demospringdata/src/main/java/me/whiteship/demospringdata/CommentRepository.java
@@ -0,0 +1,10 @@
+package me.whiteship.demospringdata;
+
+import org.springframework.data.repository.RepositoryDefinition;
+
+import java.util.List;
+
+
+public interface CommentRepository extends MyRepository<Comment, Long>{
+
+}
---
+++ demospringdata/src/test/java/me/whiteship/demospringdata/CommentRepositoryTest.java
@@ -0,0 +1,40 @@
+package me.whiteship.demospringdata;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class CommentRepositoryTest {
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Test
+    public void curd(){
+
+        Comment comment = new Comment();
+        comment.setComment("Hello Comment");
+        commentRepository.save(comment);
+
+        List<Comment> all = commentRepository.findAll();
+        assertThat(all.size()).isEqualTo(1);
+
+        long count = commentRepository.count();
+        assertThat(count).isEqualTo(1);
+
+        Optional<Comment> byId = commentRepository.findById(100l);
+        assertThat(byId.isEmpty());
+        Comment comment1 = byId.orElseThrow(IllegalArgumentException::new);
+    }
+
+}
